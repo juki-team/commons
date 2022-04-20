08e3a8fd34556fd11806e768d1cd384171e58c28
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2022-04-19 22:41:21 -0400

    0.0.36

--- src/types/submission.ts
+++ src/types/submission.ts
@@ -1,13 +1,17 @@
 import { ProblemVerdict } from './judge';
 
-export type RunResult = {
+export type DataLogType = {
   timeUsed: number,
   memoryUsed: number,
   exitCode: number,
+}
+
+export type RunResult = DataLogType & {
   log: string,
   err: string,
   out: string
 }
+
 export type CompilationRunResultType = RunResult;
 
 export type TestCaseRunResultType = Omit<RunResult, 'out'> & {
