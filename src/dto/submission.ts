d7dfa3bffe37c1c97905d054e8fae1ffb676cee9
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2022-04-20 00:15:17 -0400

    0.0.38

--- src/dto/submission.ts
+++ src/dto/submission.ts
@@ -10,34 +10,34 @@ import {
 
 export interface SubmissionResponseDTO {
   submitId: string,
-  timestamp: number,
   language: ProgrammingLanguage,
+  timestamp: number,
   memoryUsed: number,
   timeUsed: number,
   verdict: ProblemVerdict,
-  verdictByGroups: { [key: number]: TestCaseRunResultByGroupType },
+  points: number, // default: 0
   status: SubmissionRunStatus,
-  problemKey: number,
   // permissions
   canViewSourceCode: boolean, // foreign
   // problem
+  problemKey: number,
   problemName: string, // foreign
   problemMode: ProblemMode, // foreign
   problemType: ProblemType, // foreign
   problemTimeLimit: number, // foreign
   problemMemoryLimit: number, // foreign
   // User
-  userImageUrl: string, // foreign
   userNickname: string, // foreign
+  userImageUrl: string, // foreign
   // contest data or empty string
   contestKey: string,
   contestName: string, // foreign
   contestProblemIndex: string,
-  submitPoints: number, // default: 0
 }
 
 export interface SubmitResponseDTO extends SubmissionResponseDTO{
   sourceCode: string,
+  verdictByGroups: { [key: number]: TestCaseRunResultByGroupType },
   testCaseResults: TestCaseRunResultType[],
   compilationResult: CompilationRunResultType,
 }
