92427026e19d480c792c78783dcb4fcba68f569b
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2022-04-21 16:47:51 -0400

    0.0.48

--- src/dto/index.ts
+++ src/dto/index.ts
@@ -1,1 +1,2 @@
+export * from './scoreboard';
 export * from './submission';
--- src/dto/scoreboard.ts
+++ src/dto/scoreboard.ts
@@ -1,51 +1,14 @@
-import { DataLogType, ProblemMode, ProblemType, ProblemVerdict, ProgrammingLanguage, SubmissionRunStatus } from '../types';
-
-export interface SubmissionResponseDTO {
-  submitId: string,
-  language: ProgrammingLanguage,
-  timestamp: number,
-  memoryUsed: number,
-  timeUsed: number,
-  verdict: ProblemVerdict,
-  points: number, // default: 0
-  status: SubmissionRunStatus,
-  // permissions
-  canViewSourceCode: boolean, // foreign
-  // problem
-  problemKey: number,
-  problemName: string, // foreign
-  problemMode: ProblemMode, // foreign
-  problemType: ProblemType, // foreign
-  problemTimeLimit: number, // foreign
-  problemMemoryLimit: number, // foreign
-  // User
-  userNickname: string, // foreign
-  userImageUrl: string, // foreign
-  // contest data or empty string
-  contestKey: string,
-  contestName: string, // foreign
-  contestProblemIndex: string,
-}
-
-export type TestCaseResultType = DataLogType & {
-  err: string
-  verdict: ProblemVerdict,
-  group: number,
-  points: number, // Used by PARTIAL mode problems
-}
-
-export type VerdictByGroupsType = {
-  [key: number]: TestCaseResultType
-};
-
-export type CompilationResultType = DataLogType & {
-  err: string,
-  success: boolean,
-};
-
-export interface SubmitResponseDTO extends SubmissionResponseDTO {
-  sourceCode: string,
-  verdictByGroups: VerdictByGroupsType,
-  testCaseResults: TestCaseResultType[],
-  compilationResult: CompilationResultType,
+export interface ScoreboardResponseDTO {
+  userNickname: string,
+  userFamilyName: string,
+  userGivenName: string,
+  userImageUrl: string,
+  totalPenalty: number,
+  totalPoints: number,
+  problems: {
+    attempts: number,
+    points: number,
+    success: boolean,
+    penalty: number,
+  }[],
 }
