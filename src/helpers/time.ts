d8e6adcfa8a9419da78db410a745dca780adae14
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2022-04-30 14:12:30 -0400

    0.0.57

--- src/helpers/time.ts
+++ src/helpers/time.ts
@@ -1,32 +1,34 @@
-export const splitTime = (timeRemaining: number): Array<{ remaining: number, label: string }> => {
-  const remaining: Array<{ remaining: number, label: string }> = [];
+type SplitTime = { remaining: number, label: string, milliseconds: number };
+
+export const splitTime = (timeRemaining: number): Array<SplitTime> => {
+  const remaining: Array<SplitTime> = [];
   
   const aYearMilliseconds = 1000 * 60 * 60 * 24 * 365;
   const remainingYears = Math.floor(timeRemaining / aYearMilliseconds);
-  remaining.push({ remaining: remainingYears, label: 'years' });
+  remaining.push({ remaining: remainingYears, label: 'years', milliseconds: aYearMilliseconds });
   
   const aWeekMilliseconds = 1000 * 60 * 60 * 24 * 7;
   const remainingWeeks = Math.floor((timeRemaining % aYearMilliseconds) / aWeekMilliseconds);
-  remaining.push({ remaining: remainingWeeks, label: 'weeks' });
+  remaining.push({ remaining: remainingWeeks, label: 'weeks', milliseconds: aWeekMilliseconds });
   
   const aDayMilliseconds = 1000 * 60 * 60 * 24;
   const remainingDays = Math.floor((timeRemaining % aWeekMilliseconds) / aDayMilliseconds);
-  remaining.push({ remaining: remainingDays, label: 'days' });
+  remaining.push({ remaining: remainingDays, label: 'days', milliseconds: aDayMilliseconds });
   
   const aHourMilliseconds = 1000 * 60 * 60;
   const remainingHours = Math.floor((timeRemaining % aDayMilliseconds) / aHourMilliseconds);
-  remaining.push({ remaining: remainingHours, label: 'hours' });
+  remaining.push({ remaining: remainingHours, label: 'hours', milliseconds: aHourMilliseconds });
   
   const aMinuteMilliseconds = 1000 * 60;
   const remainingMinutes = Math.floor((timeRemaining % aHourMilliseconds) / aMinuteMilliseconds);
-  remaining.push({ remaining: remainingMinutes, label: 'minutes' });
+  remaining.push({ remaining: remainingMinutes, label: 'minutes', milliseconds: aMinuteMilliseconds });
   
   const aSecondMilliseconds = 1000;
   const remainingSeconds = Math.floor((timeRemaining % aMinuteMilliseconds) / aSecondMilliseconds);
-  remaining.push({ remaining: remainingSeconds, label: 'seconds' });
+  remaining.push({ remaining: remainingSeconds, label: 'seconds', milliseconds: aSecondMilliseconds });
   
   const remainingMilliseconds = Math.floor(timeRemaining % aSecondMilliseconds);
-  remaining.push({ remaining: remainingMilliseconds, label: 'milliseconds' });
+  remaining.push({ remaining: remainingMilliseconds, label: 'milliseconds', milliseconds: 1 });
   
   while (remaining[0].remaining <= 0) {
     remaining.shift();
