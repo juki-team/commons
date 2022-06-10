2271a349e3d95f4dd011198689d073e8fcfc1ab1
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2022-06-10 00:11:36 -0400

    0.0.80

---
+++ src/dto/user.ts
@@ -0,0 +1,14 @@
+import { UserStatus } from '../types';
+
+export interface userSummaryResponseDTO {
+  aboutMe: string,
+  city: string,
+  country: string,
+  email: string,
+  familyName: string,
+  givenName: string,
+  imageUrl: string,
+  institution: string,
+  nickname: string,
+  status: UserStatus,
+}
