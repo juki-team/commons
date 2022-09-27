61522091618ee5276431946d9afd46de6ebfa558
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2022-09-27 17:57:03 -0400

    0.0.141

--- src/constants/services.ts
+++ src/constants/services.ts
@@ -12,39 +12,64 @@ export const ERROR: { [key in ErrorCode]: { value: ErrorCode, message: string, s
   [ErrorCode.ERR9997]: { value: ErrorCode.ERR9997, message: 'aborted request', status: 400 },
   [ErrorCode.ERR9998]: { value: ErrorCode.ERR9998, message: 'error on response', status: 400 },
   [ErrorCode.ERR9999]: { value: ErrorCode.ERR9999, message: 'service no found', status: 400 },
-  // User errors // 'unable update nickname yet'}, 'User not active'
+  // USER ERRORS ERR00XX
   [ErrorCode.ERR0000]: { value: ErrorCode.ERR0000, message: 'internal server error on users service', status: 500 },
-  [ErrorCode.ERR0001]: { value: ErrorCode.ERR0001, message: 'user not found', status: 400 },
-  [ErrorCode.ERR0002]: { value: ErrorCode.ERR0002, message: 'user is reported', status: 400 },
-  [ErrorCode.ERR0003]: { value: ErrorCode.ERR0003, message: 'user is archived', status: 400 },
-  [ErrorCode.ERR0004]: { value: ErrorCode.ERR0004, message: 'user with email requested not found', status: 400 },
-  [ErrorCode.ERR0005]: { value: ErrorCode.ERR0005, message: 'user with nickname requested not found', status: 400 },
-  [ErrorCode.ERR0006]: { value: ErrorCode.ERR0006, message: 'user session not found', status: 401 },
-  [ErrorCode.ERR0010]: { value: ErrorCode.ERR0010, message: 'the email is already registered', status: 400 },
-  [ErrorCode.ERR0011]: { value: ErrorCode.ERR0011, message: 'the nickname is already taken', status: 400 },
-  [ErrorCode.ERR0012]: { value: ErrorCode.ERR0012, message: 'incorrect password', status: 400 },
-  // Problem Errors ERR01XX
+  // User server errors
+  [ErrorCode.ERR0010]: { value: ErrorCode.ERR0010, message: 'user not created', status: 500 },
+  [ErrorCode.ERR0011]: { value: ErrorCode.ERR0011, message: 'user not found', status: 500 },
+  [ErrorCode.ERR0012]: { value: ErrorCode.ERR0012, message: 'user not updated', status: 500 },
+  [ErrorCode.ERR0013]: { value: ErrorCode.ERR0013, message: 'user not deleted', status: 500 },
+  // User user permission error
+  [ErrorCode.ERR0014]: { value: ErrorCode.ERR0014, message: 'not allowed to create the user', status: 400 },
+  [ErrorCode.ERR0015]: { value: ErrorCode.ERR0015, message: 'not allowed to view the user', status: 400 },
+  [ErrorCode.ERR0016]: { value: ErrorCode.ERR0016, message: 'not allowed to updated the user', status: 400 },
+  [ErrorCode.ERR0017]: { value: ErrorCode.ERR0017, message: 'not allowed to deleted the user', status: 400 },
+  // User others permissions errors
+  [ErrorCode.ERR0018]: { value: ErrorCode.ERR0018, message: 'user is reported', status: 400 },
+  [ErrorCode.ERR0019]: { value: ErrorCode.ERR0019, message: 'user is archived', status: 400 },
+  [ErrorCode.ERR0020]: { value: ErrorCode.ERR0020, message: 'user with email requested not found', status: 400 },
+  [ErrorCode.ERR0021]: { value: ErrorCode.ERR0021, message: 'user with nickname requested not found', status: 400 },
+  [ErrorCode.ERR0022]: { value: ErrorCode.ERR0022, message: 'the email is already registered', status: 400 },
+  [ErrorCode.ERR0023]: { value: ErrorCode.ERR0023, message: 'the nickname is already taken', status: 400 },
+  [ErrorCode.ERR0024]: { value: ErrorCode.ERR0024, message: 'incorrect password', status: 400 },
+  // User sessions errors
+  [ErrorCode.ERR0050]: { value: ErrorCode.ERR0050, message: 'user session not created', status: 500 },
+  [ErrorCode.ERR0051]: { value: ErrorCode.ERR0051, message: 'user session not found', status: 500 },
+  [ErrorCode.ERR0052]: { value: ErrorCode.ERR0052, message: 'user session not updated', status: 500 },
+  [ErrorCode.ERR0053]: { value: ErrorCode.ERR0053, message: 'user session not deleted', status: 500 },
+  // PROBLEM ERRORS ERR01XX
   [ErrorCode.ERR0100]: { value: ErrorCode.ERR0100, message: 'internal server error on problems service', status: 500 },
-  [ErrorCode.ERR0101]: { value: ErrorCode.ERR0101, message: 'problem not found', status: 400 },
-  [ErrorCode.ERR0110]: { value: ErrorCode.ERR0110, message: 'problem mode not supported', status: 500 },
-  [ErrorCode.ERR0111]: { value: ErrorCode.ERR0111, message: 'problem type not supported', status: 500 },
-  [ErrorCode.ERR0112]: { value: ErrorCode.ERR0112, message: 'problem time limit must be a number', status: 500 },
-  [ErrorCode.ERR0113]: { value: ErrorCode.ERR0113, message: 'problem memory limit must be number', status: 500 },
+  // Problem server errors
+  [ErrorCode.ERR0110]: { value: ErrorCode.ERR0110, message: 'problem not created', status: 500 },
+  [ErrorCode.ERR0111]: { value: ErrorCode.ERR0111, message: 'problem not found', status: 500 },
+  [ErrorCode.ERR0112]: { value: ErrorCode.ERR0112, message: 'problem not updated', status: 500 },
+  [ErrorCode.ERR0113]: { value: ErrorCode.ERR0113, message: 'problem not deleted', status: 500 },
+  // Problem user permission error
+  [ErrorCode.ERR0114]: { value: ErrorCode.ERR0114, message: 'not allowed to create the problem', status: 400 },
+  [ErrorCode.ERR0115]: { value: ErrorCode.ERR0115, message: 'not allowed to view the problem', status: 400 },
+  [ErrorCode.ERR0116]: { value: ErrorCode.ERR0116, message: 'not allowed to updated the problem', status: 400 },
+  [ErrorCode.ERR0117]: { value: ErrorCode.ERR0117, message: 'not allowed to deleted the problem', status: 400 },
+  // Problem others permissions errors
+  [ErrorCode.ERR0118]: { value: ErrorCode.ERR0118, message: 'problem mode not supported', status: 500 },
+  [ErrorCode.ERR0119]: { value: ErrorCode.ERR0119, message: 'problem type not supported', status: 500 },
+  [ErrorCode.ERR0120]: { value: ErrorCode.ERR0120, message: 'problem time limit must be a number', status: 500 },
+  [ErrorCode.ERR0121]: { value: ErrorCode.ERR0121, message: 'problem memory limit must be number', status: 500 },
+  
   // Contest Errors ERR02XX
   [ErrorCode.ERR0200]: { value: ErrorCode.ERR0200, message: 'internal server error on contests service', status: 500 },
-  
+  // Contest Server Error
   [ErrorCode.ERR0210]: { value: ErrorCode.ERR0210, message: 'contest not created', status: 500 },
   [ErrorCode.ERR0211]: { value: ErrorCode.ERR0211, message: 'contest not found', status: 500 },
   [ErrorCode.ERR0212]: { value: ErrorCode.ERR0212, message: 'contest not updated', status: 500 },
   [ErrorCode.ERR0213]: { value: ErrorCode.ERR0213, message: 'contest not deleted', status: 500 },
-  
+  // Contest User Permission error
   [ErrorCode.ERR0214]: { value: ErrorCode.ERR0214, message: 'not allowed to create the contest', status: 400 },
   [ErrorCode.ERR0215]: { value: ErrorCode.ERR0215, message: 'not allowed to view the contest', status: 400 },
   [ErrorCode.ERR0216]: { value: ErrorCode.ERR0216, message: 'not allowed to updated the contest', status: 400 },
   [ErrorCode.ERR0217]: { value: ErrorCode.ERR0217, message: 'not allowed to deleted the contest', status: 400 },
+  // Contest Others permissions errors
   [ErrorCode.ERR0218]: { value: ErrorCode.ERR0218, message: 'not allowed to register to the contest', status: 400 },
   [ErrorCode.ERR0219]: { value: ErrorCode.ERR0219, message: 'is already registered', status: 400 },
-  
   [ErrorCode.ERR0220]: { value: ErrorCode.ERR0220, message: 'not allowed to view contests', status: 400 },
   
   [ErrorCode.ERR0230]: { value: ErrorCode.ERR0230, message: 'scoreboard not created', status: 500 },
