493e9d52d9e6f2cb63c44fbacff59835e13874ec
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2023-02-14 15:26:10 -0400

    0.0.235

--- src/constants/services.ts
+++ src/constants/services.ts
@@ -8,17 +8,20 @@ export const ERROR: { [key in ErrorCode]: { value: ErrorCode, message: string, s
   [ErrorCode.ERR403]: { value: ErrorCode.ERR403, message: 'forbidden', status: 403 },
   [ErrorCode.ERR404]: { value: ErrorCode.ERR404, message: 'not found', status: 404 },
   [ErrorCode.ERR405]: { value: ErrorCode.ERR405, message: 'method not allowed', status: 405 },
+  
   // UI general errors
   [ErrorCode.ERR9997]: { value: ErrorCode.ERR9997, message: 'aborted request', status: 400 },
   [ErrorCode.ERR9998]: { value: ErrorCode.ERR9998, message: 'error on response', status: 400 },
   [ErrorCode.ERR9999]: { value: ErrorCode.ERR9999, message: 'service no found', status: 400 },
-  // USER ERRORS ERR99XX
+  
+  // COMPANY ERRORS ERR99XX
   [ErrorCode.ERR9900]: { value: ErrorCode.ERR0000, message: 'internal server error on company service', status: 500 },
-  // User server errors
+  // Company server errors
   [ErrorCode.ERR9910]: { value: ErrorCode.ERR9910, message: 'company not created', status: 500 },
   [ErrorCode.ERR9911]: { value: ErrorCode.ERR9911, message: 'company not found', status: 500 },
   [ErrorCode.ERR9912]: { value: ErrorCode.ERR9912, message: 'company not updated', status: 500 },
   [ErrorCode.ERR9913]: { value: ErrorCode.ERR9913, message: 'company not deleted', status: 500 },
+  
   // USER ERRORS ERR00XX
   [ErrorCode.ERR0000]: { value: ErrorCode.ERR0000, message: 'internal server error on users service', status: 500 },
   // User server errors
@@ -31,9 +34,10 @@ export const ERROR: { [key in ErrorCode]: { value: ErrorCode, message: string, s
   [ErrorCode.ERR0015]: { value: ErrorCode.ERR0015, message: 'not allowed to view the user', status: 400 },
   [ErrorCode.ERR0016]: { value: ErrorCode.ERR0016, message: 'not allowed to updated the user', status: 400 },
   [ErrorCode.ERR0017]: { value: ErrorCode.ERR0017, message: 'not allowed to deleted the user', status: 400 },
-  // User others permissions errors
+  // User status permissions errors
   [ErrorCode.ERR0018]: { value: ErrorCode.ERR0018, message: 'user is reported', status: 400 },
   [ErrorCode.ERR0019]: { value: ErrorCode.ERR0019, message: 'user is archived', status: 400 },
+  // User others permissions errors
   [ErrorCode.ERR0020]: { value: ErrorCode.ERR0020, message: 'user with email requested not found', status: 400 },
   [ErrorCode.ERR0021]: { value: ErrorCode.ERR0021, message: 'user with nickname requested not found', status: 400 },
   [ErrorCode.ERR0022]: { value: ErrorCode.ERR0022, message: 'the email is already registered', status: 400 },
@@ -44,6 +48,7 @@ export const ERROR: { [key in ErrorCode]: { value: ErrorCode, message: string, s
   [ErrorCode.ERR0051]: { value: ErrorCode.ERR0051, message: 'user session not found', status: 500 },
   [ErrorCode.ERR0052]: { value: ErrorCode.ERR0052, message: 'user session not updated', status: 500 },
   [ErrorCode.ERR0053]: { value: ErrorCode.ERR0053, message: 'user session not deleted', status: 500 },
+  
   // PROBLEM ERRORS ERR01XX
   [ErrorCode.ERR0100]: { value: ErrorCode.ERR0100, message: 'internal server error on problems service', status: 500 },
   // Problem server errors
@@ -114,9 +119,35 @@ export const ERROR: { [key in ErrorCode]: { value: ErrorCode, message: string, s
   [ErrorCode.ERR0421]: { value: ErrorCode.ERR0421, message: 'compilation failed', status: 400 }, // codes
   [ErrorCode.ERR0430]: { value: ErrorCode.ERR0430, message: '', status: 400 }, // files
   [ErrorCode.ERR0440]: { value: ErrorCode.ERR0440, message: 'the email could not be sent', status: 400 }, // mails
-  // Team Errors ERR06XX
+  
+  // TEAMS ERRORS ERR06XX
   [ErrorCode.ERR0600]: { value: ErrorCode.ERR0600, message: 'internal server error on teams service', status: 500 },
-  [ErrorCode.ERR0601]: { value: ErrorCode.ERR0601, message: 'team not found', status: 400 },
-  [ErrorCode.ERR0602]: { value: ErrorCode.ERR0602, message: 'team is reported', status: 400 },
-  [ErrorCode.ERR0603]: { value: ErrorCode.ERR0603, message: 'team is archived', status: 400 },
+  // Teams server errors
+  [ErrorCode.ERR0610]: { value: ErrorCode.ERR0610, message: 'team not created', status: 400 },
+  [ErrorCode.ERR0611]: { value: ErrorCode.ERR0611, message: 'team not found', status: 400 },
+  [ErrorCode.ERR0612]: { value: ErrorCode.ERR0612, message: 'team not updated', status: 400 },
+  [ErrorCode.ERR0613]: { value: ErrorCode.ERR0613, message: 'team not deleted', status: 400 },
+  // Teams user permission error
+  [ErrorCode.ERR0614]: { value: ErrorCode.ERR0614, message: 'not allowed to create the team', status: 400 },
+  [ErrorCode.ERR0615]: { value: ErrorCode.ERR0615, message: 'not allowed to view the team', status: 400 },
+  [ErrorCode.ERR0616]: { value: ErrorCode.ERR0616, message: 'not allowed to updated the team', status: 400 },
+  [ErrorCode.ERR0617]: { value: ErrorCode.ERR0617, message: 'not allowed to deleted the team', status: 400 },
+  // Teams status permission error
+  [ErrorCode.ERR0618]: { value: ErrorCode.ERR0618, message: 'team is reported', status: 400 },
+  [ErrorCode.ERR0619]: { value: ErrorCode.ERR0619, message: 'team is archived', status: 400 },
+  
+  // SHEETS ERRORS ERR06XX
+  [ErrorCode.ERR0800]: { value: ErrorCode.ERR0800, message: 'internal server error on sheets service', status: 500 },
+  // Sheets server errors
+  [ErrorCode.ERR0810]: { value: ErrorCode.ERR0810, message: 'sheet not created', status: 400 },
+  [ErrorCode.ERR0811]: { value: ErrorCode.ERR0811, message: 'sheet not found', status: 400 },
+  [ErrorCode.ERR0812]: { value: ErrorCode.ERR0812, message: 'sheet not updated', status: 400 },
+  [ErrorCode.ERR0813]: { value: ErrorCode.ERR0813, message: 'sheet not deleted', status: 400 },
+  // Sheets user permission error
+  [ErrorCode.ERR0814]: { value: ErrorCode.ERR0814, message: 'not allowed to create the sheet', status: 400 },
+  [ErrorCode.ERR0815]: { value: ErrorCode.ERR0815, message: 'not allowed to view the sheet', status: 400 },
+  [ErrorCode.ERR0816]: { value: ErrorCode.ERR0816, message: 'not allowed to updated the sheet', status: 400 },
+  [ErrorCode.ERR0817]: { value: ErrorCode.ERR0817, message: 'not allowed to deleted the sheet', status: 400 },
+  // Sheets status permission error
+  [ErrorCode.ERR0818]: { value: ErrorCode.ERR0818, message: 'sheet is private', status: 400 },
 };
