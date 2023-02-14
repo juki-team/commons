7079927d42fc4a3936add89882a5245fbf64501e
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2023-02-14 12:14:37 -0400

    v0.0.229

---
+++ src/dto/sheet.ts
@@ -0,0 +1,54 @@
+import {
+  ContestClarificationType,
+  ContestMembersResponseType,
+  ContestProblemBasicType,
+  ContestProblemType,
+  ContestSettingsBasicType,
+  ContestStatus,
+  ContestUserType,
+  CreateContestMembersBasicType,
+} from '../types';
+
+export interface CreateContestDTO {
+  key: string,
+  name: string,
+  description: string,
+  settings: ContestSettingsBasicType,
+  problems: { [key: string]: ContestProblemBasicType },
+  members: CreateContestMembersBasicType,
+  tags: string[],
+  status: ContestStatus,
+}
+
+export interface ContestSummaryListResponseDTO {
+  status: ContestStatus,
+  name: string,
+  key: string,
+  user: ContestUserType,
+  settings: {
+    startTimestamp: number,
+    endTimestamp: number,
+    // To get the contest template
+    frozenTimestamp: number,
+    quietTimestamp: number,
+    penalty: number,
+  },
+  tags: string[],
+  // Data Calculated
+  totalContestants: number,
+  isLive: boolean,
+  isPast: boolean,
+  isFuture: boolean,
+  isEndless: boolean,
+  isFrozenTime: boolean,
+  isQuietTime: boolean,
+}
+
+export interface ContestResponseDTO extends ContestSummaryListResponseDTO {
+  description: string,
+  settings: ContestSettingsBasicType & { scoreboardLocked: boolean, },
+  problems: { [key: string]: ContestProblemType },
+  user: ContestUserType,
+  members: ContestMembersResponseType,
+  clarifications: ContestClarificationType[]
+}
