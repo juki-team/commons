6d406f46a2d9bcffb20a3995fea932f588882155
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2023-02-25 11:25:15 -0400

    0.0.241

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 export * from './account';
 export * from './commons';
 export * from './contest';
+export * from './folder';
 export * from './judge';
 export * from './prisma';
 export * from './problems';
