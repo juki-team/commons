aafc8adb442161ed914e945bd46122d5d5f4f980
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2023-04-09 12:00:26 -0400

    Adding SocketEventRunResponseDTO

---
+++ src/dto/socket.ts
@@ -0,0 +1,10 @@
+import { SubmissionRunStatus } from '../types';
+
+export type InfoLogCaseStatus = { inputKey: string, out: string, err: string, log: string };
+
+export interface SocketEventRunResponseDTO {
+  runId: string,
+  messageTimestamp: number,
+  status: SubmissionRunStatus,
+  log?: InfoLogCaseStatus
+}
