3600c031dc3d4fd64d91253d9958a70c8c809466
Oscar Gauss <me@oscargauss.com>
2024-02-08 02:31:06 -0400

    Adding isRunnerListener v2

--- src/dto/system.ts
+++ src/dto/system.ts
@@ -16,6 +16,7 @@ export interface TaskResponseDTO {
   lastStatus: string,
   isLowRunnerGroup: boolean,
   isHighRunnerGroup: boolean,
+  isRunnerListener: boolean,
   containers?: TaskResponseDTOContainer[];
 }
 
