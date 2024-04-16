89effafa2231cca11e897add5faf473afa68449f
Oscar Gauss <me@oscargauss.com>
2024-04-15 23:39:39 -0400

    fixing types

--- src/types/status.ts
+++ src/types/status.ts
@@ -1,5 +1,7 @@
-import { CodeEditorSheetType, JkmdSheetType } from './sheet';
+import { CodeEditorSheetType, JkmdSheetType, SheetType } from './sheet';
 
-export type CodeEditorSheetStatusType = Omit<CodeEditorSheetType, 'height' | 'languages'>;
+export type QuizProblemSheetStatusType = Omit<CodeEditorSheetType, 'height' | 'languages' | 'type'> & {
+  type: SheetType.QUIZ_PROBLEM
+};
 
 export type JkmdSheetStatusType = Omit<JkmdSheetType, 'content'> & { read: boolean };
