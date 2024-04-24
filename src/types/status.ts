5bf5bba92c260eeb732fb5b0275119e82b134111
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2024-04-24 16:57:42 -0400

    fix

--- src/types/status.ts
+++ src/types/status.ts
@@ -1,5 +1,9 @@
-import { JkmdSheetType, QuizProblemSheetType } from './sheet';
+import { JkmdSheetType, QuizProblemSheetType, SourceCodeType } from './sheet';
 
-export type QuizProblemSheetStatusType = Omit<QuizProblemSheetType, 'height' | 'languages' | 'title' | 'description' | 'solutionSourceCode' | 'points' | 'scoringMode'>;
+export type QuizProblemSheetStatusType =
+  Omit<QuizProblemSheetType, 'height' | 'languages' | 'title' | 'description' | 'solutionSourceCode' | 'points' | 'scoringMode'>
+  & {
+  sourceCode: SourceCodeType,
+};
 
 export type JkmdSheetStatusType = Omit<JkmdSheetType, 'content' | 'title'> & { read: boolean };
