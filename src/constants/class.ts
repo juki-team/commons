2139e2f6e79ff3c319dc00db4d45db22e09886c5
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2024-06-28 03:51:10 -0400

    adding CLASS_CYCLE_STATE

--- src/constants/class.ts
+++ src/constants/class.ts
@@ -1,4 +1,4 @@
-import { ClassState } from '../types';
+import { ClassCycleState, ClassState } from '../types';
 
 export const CLASS_STATE: { [key in ClassState]: { value: ClassState, label: string, description: string } } = {
   [ClassState.RELEASED]: {
@@ -10,5 +10,24 @@ export const CLASS_STATE: { [key in ClassState]: { value: ClassState, label: str
     value: ClassState.ARCHIVED,
     label: 'archived',
     description: 'the class will not appear for anyone, contact the administrator to see it again',
+  },
+};
+
+export const CLASS_CYCLE_STATE: {
+  [key in ClassCycleState]: {
+    value: ClassCycleState,
+    label: string,
+    description: string
+  }
+} = {
+  [ClassCycleState.RELEASED]: {
+    value: ClassCycleState.RELEASED,
+    label: 'released',
+    description: 'the cycle will be released',
+  },
+  [ClassCycleState.ARCHIVED]: {
+    value: ClassCycleState.ARCHIVED,
+    label: 'archived',
+    description: 'the cycle will not appear for anyone, contact the administrator to see it again',
   },
 };
