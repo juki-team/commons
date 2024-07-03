c8e62b4a2812bf61f2fce0a8aea9dd12c8753879
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2024-07-03 14:04:04 -0400

    fixing getDocumentAccess

--- src/helpers/document.ts
+++ src/helpers/document.ts
@@ -6,31 +6,31 @@ import {
   EntityUsersMemberUserData,
 } from '../types';
 
-export const getDocumentAccess = (document: { members: EntityMembers }): EntityAccess => {
+export const getDocumentAccess = (document: { members: EntityMembers } | undefined): EntityAccess => {
   switch (true) {
-    case document.members?.rankAdministrators === EntityMembersRank.NONE
-    && document.members?.rankManagers === EntityMembersRank.OPEN
-    && document.members?.rankGuests === EntityMembersRank.NONE
-    && document.members?.rankSpectators === EntityMembersRank.OPEN
-    && document.members?.rankParticipants === EntityMembersRank.NONE:
+    case document?.members?.rankAdministrators === EntityMembersRank.NONE
+    && document?.members?.rankManagers === EntityMembersRank.OPEN
+    && document?.members?.rankGuests === EntityMembersRank.NONE
+    && document?.members?.rankSpectators === EntityMembersRank.OPEN
+    && document?.members?.rankParticipants === EntityMembersRank.NONE:
       return EntityAccess.EXPOSED;
-    case document.members?.rankAdministrators === EntityMembersRank.NONE
-    && document.members?.rankManagers === EntityMembersRank.CLOSE
-    && document.members?.rankGuests === EntityMembersRank.NONE
-    && document.members?.rankSpectators === EntityMembersRank.OPEN
-    && document.members?.rankParticipants === EntityMembersRank.NONE:
+    case document?.members?.rankAdministrators === EntityMembersRank.NONE
+    && document?.members?.rankManagers === EntityMembersRank.CLOSE
+    && document?.members?.rankGuests === EntityMembersRank.NONE
+    && document?.members?.rankSpectators === EntityMembersRank.OPEN
+    && document?.members?.rankParticipants === EntityMembersRank.NONE:
       return EntityAccess.PUBLIC;
-    case document.members?.rankAdministrators === EntityMembersRank.NONE
-    && document.members?.rankManagers === EntityMembersRank.CLOSE
-    && document.members?.rankGuests === EntityMembersRank.NONE
-    && document.members?.rankSpectators === EntityMembersRank.CLOSE
-    && document.members?.rankParticipants === EntityMembersRank.NONE:
+    case document?.members?.rankAdministrators === EntityMembersRank.NONE
+    && document?.members?.rankManagers === EntityMembersRank.CLOSE
+    && document?.members?.rankGuests === EntityMembersRank.NONE
+    && document?.members?.rankSpectators === EntityMembersRank.CLOSE
+    && document?.members?.rankParticipants === EntityMembersRank.NONE:
       return EntityAccess.RESTRICTED;
-    case document.members?.rankAdministrators === EntityMembersRank.NONE
-    && document.members?.rankManagers === EntityMembersRank.NONE
-    && document.members?.rankGuests === EntityMembersRank.NONE
-    && document.members?.rankSpectators === EntityMembersRank.NONE
-    && document.members?.rankParticipants === EntityMembersRank.NONE:
+    case document?.members?.rankAdministrators === EntityMembersRank.NONE
+    && document?.members?.rankManagers === EntityMembersRank.NONE
+    && document?.members?.rankGuests === EntityMembersRank.NONE
+    && document?.members?.rankSpectators === EntityMembersRank.NONE
+    && document?.members?.rankParticipants === EntityMembersRank.NONE:
       return EntityAccess.PRIVATE;
     default:
       return EntityAccess.PRIVATE;
