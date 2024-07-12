1a7086cbc8ebdcfdcbcaf333fce51d42a5e1e9c6
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2024-07-11 21:54:51 -0400

    fixing ProblemDataSystemResponseDTO

--- src/dto/problem.ts
+++ src/dto/problem.ts
@@ -35,7 +35,7 @@ export interface ProblemDataResponseDTO extends ProblemSummaryListResponseDTO {
   members: DocumentMembersResponseDTO,
 }
 
-export interface ProblemDataSystemResponseDTO extends ProblemDataResponseDTO {
+export interface ProblemDataSystemResponseDTO extends ProblemSummaryListResponseDTO, Omit<ProblemDataResponseDTO, 'settings'> {
   state: EntityState,
   id: string,
   ownerUserId: string,
