ee97fb35d6e18843e4def9af53598644d566e043
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2024-07-18 10:06:35 -0400

    fixing JudgeSystemSummaryListResponseDTO

--- src/dto/judge.ts
+++ src/dto/judge.ts
@@ -6,7 +6,7 @@ export interface JudgeDataResponseDTO extends JudgeBaseDocument {
 export interface JudgeSummaryListResponseDTO extends JudgeBaseDocument {
 }
 
-export interface JudgeSystemSummaryListResponseDTO extends JudgeBaseDocument {
+export interface JudgeSystemSummaryListResponseDTO extends JudgeSummaryListResponseDTO {
   id: string,
   creationTimestamp: number,
   updateTimestamp: number,
