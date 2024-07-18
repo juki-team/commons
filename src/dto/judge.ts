5da97f8c7f21ef3c90fa40bebeab010029a4acea
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2024-07-18 09:39:02 -0400

    adding JudgeSystemSummaryListResponseDTO

--- src/dto/judge.ts
+++ src/dto/judge.ts
@@ -2,3 +2,9 @@ import { JudgeBaseDocument } from '../types';
 
 export interface JudgeDataResponseDTO extends JudgeBaseDocument {
 }
+
+export interface JudgeSystemSummaryListResponseDTO extends JudgeBaseDocument {
+  id: string,
+  creationTimestamp: number,
+  updateTimestamp: number,
+}
