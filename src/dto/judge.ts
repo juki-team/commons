ebfe4be54e172e66cb25393896127b18489a5bf1
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2024-10-11 13:02:42 -0400

    fixing dto judge

--- src/dto/judge.ts
+++ src/dto/judge.ts
@@ -1,13 +1,19 @@
 import { JudgeBaseDocument } from '../types';
 
-export interface JudgeDataResponseDTO extends JudgeBaseDocument {
+export interface JudgeDataResponseDTO extends Omit<JudgeBaseDocument, 'getProblemUrl' | 'getUserSubmissionsUrl' | 'getSubmissionUrl' | 'getLoginUrl' | 'getProfileUrl' | 'getSubmitUrl'> {
 }
 
-export interface JudgeSummaryListResponseDTO extends JudgeBaseDocument {
+export interface JudgeSummaryListResponseDTO extends Omit<JudgeBaseDocument, 'getProblemUrl' | 'getUserSubmissionsUrl' | 'getSubmissionUrl' | 'getLoginUrl' | 'getProfileUrl' | 'getSubmitUrl'> {
 }
 
 export interface JudgeSystemSummaryListResponseDTO extends JudgeSummaryListResponseDTO {
   id: string,
   creationTimestamp: number,
   updateTimestamp: number,
+  getLoginUrl: string,
+  getProfileUrl: string,
+  getSubmitUrl: string,
+  getSubmissionUrl: string,
+  getProblemUrl: string,
+  getUserSubmissionsUrl: string,
 }
