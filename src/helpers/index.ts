8ec2fe2a146f9bcf7574889cf7266f4aa26426d6
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2024-10-17 09:35:00 -0400

    adding object fun

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,6 +2,7 @@ export * from './class';
 export * from './commons';
 export * from './document';
 export * from './judging';
+export * from './object';
 export * from './responses';
 export * from './time';
 export * from './worksheet';
