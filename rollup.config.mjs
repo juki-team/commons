bc786e0bf4f57278857ec780a4621b896ae53a4e
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2024-11-26 00:57:10 -0400

    upd nodejs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,22 +1,22 @@
 import typescript from 'rollup-plugin-typescript2';
-import pkg from './package.json' assert { type: 'json' };
+import pkg from './package.json' with {type: 'json'};
 
 export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      // file: './dist/cjs/index.js',
-      dir: 'dist/cjs',
-      format: 'cjs',
-    },
-    {
-      // file: './dist/esm/index.js',
-      dir: 'dist/esm',
-      format: 'es',
-    },
-  ],
-  external: [ ...Object.keys(pkg.peerDependencies || {}) ],
-  plugins: [
-    typescript({ useTsconfigDeclarationDir: true }),
-  ],
+    input: 'src/index.ts',
+    output: [
+        {
+            // file: './dist/cjs/index.js',
+            dir: 'dist/cjs',
+            format: 'cjs',
+        },
+        {
+            // file: './dist/esm/index.js',
+            dir: 'dist/esm',
+            format: 'es',
+        },
+    ],
+    external: [...Object.keys(pkg.peerDependencies || {})],
+    plugins: [
+        typescript({useTsconfigDeclarationDir: true}),
+    ],
 };
