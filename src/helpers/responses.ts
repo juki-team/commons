343d148e9a52f954638dc81e8ee6e87c73f686e2
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2024-12-10 13:46:10 -0400

    adding cleanRequest

--- src/helpers/responses.ts
+++ src/helpers/responses.ts
@@ -1,5 +1,7 @@
+import { ERROR } from '../constants';
 import { JkError } from '../prototypes';
 import { ContentResponseType, ContentsMetaType, ContentsResponseType, ErrorCode, ErrorResponseType } from '../types';
+import { consoleError, isStringJson } from './commons';
 
 export function toJkError(err: any): JkError {
   const error = new Error();
@@ -38,3 +40,51 @@ export function contentsResponse<T>(message: string, contents: T[], meta: Conten
     meta,
   };
 }
+
+export const cleanRequest = <T extends ContentResponseType<any> | ContentsResponseType<any>>(responseText: string): (ErrorResponseType | T) => {
+  if (!isStringJson(responseText)) {
+    // this occurs when the endpoint don't exits or server is down
+    const response: ErrorResponseType = {
+      success: false,
+      message: ERROR[ErrorCode.ERR9999].message,
+      errors: [ { code: ErrorCode.ERR9999, detail: '', message: ERROR[ErrorCode.ERR9999].message } ],
+    };
+    consoleError({ message: 'success false on cleaning request', responseText, response });
+    // jukiApiManager.reportError({ message: 'success false on cleaning request', responseText, response });
+    return response;
+  }
+  const responseJson = JSON.parse(responseText);
+  if (typeof responseJson.success === 'boolean') {
+    if (responseJson.success === true && typeof responseJson.message === 'string' && responseJson.content) { // V1
+      return {
+        success: true,
+        message: responseJson.message,
+        content: responseJson.content,
+      } as T;
+    } else if (responseJson.success === true && typeof responseJson.message === 'string' && Array.isArray(responseJson.contents) && responseJson.meta) { // V1
+      return {
+        success: true,
+        message: responseJson.message,
+        contents: responseJson.contents,
+        meta: responseJson.meta,
+      } as T;
+    } else if (responseJson.success === false && typeof responseJson.message === 'string' && Array.isArray(responseJson.errors)) { // V1
+      const response: ErrorResponseType = {
+        success: false,
+        message: responseJson.message,
+        errors: responseJson.errors,
+      };
+      consoleError({ message: 'success false on cleaning request', responseText, response });
+      // jukiApiManager.reportError({ message: 'success false on cleaning request', responseText, response });
+      return response;
+    }
+  }
+  const response: ErrorResponseType = {
+    success: false,
+    message: ERROR[ErrorCode.ERR9998].message,
+    errors: [ { code: ErrorCode.ERR9998, detail: '', message: ERROR[ErrorCode.ERR9998].message } ],
+  };
+  consoleError({ message: 'success false on cleaning request', responseText, response });
+  // jukiApiManager.reportError({ message: 'success false on cleaning request', responseText, response });
+  return response;
+};
