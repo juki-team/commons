96f43ef683a465304bd6481a81636ff364c656b0
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-01-22 14:24:57 -0400

    fixing contest helpers

--- src/helpers/contest.ts
+++ src/helpers/contest.ts
@@ -1,7 +1,7 @@
 import { MAX_DATE, MIN_DATE } from '../constants';
 import { ContestSettings } from '../types';
 
-export const isEndlessContest = (settings: ContestSettings | undefined) => {
+export const isEndlessContest = (settings: Pick<ContestSettings, 'startTimestamp' | 'frozenTimestamp' | 'quietTimestamp' | 'endTimestamp' | 'penalty'> | undefined) => {
   return settings?.startTimestamp === MIN_DATE.getTime()
     && settings?.frozenTimestamp === MAX_DATE.getTime()
     && settings?.quietTimestamp === MAX_DATE.getTime()
@@ -9,7 +9,7 @@ export const isEndlessContest = (settings: ContestSettings | undefined) => {
     && settings?.penalty === 0;
 };
 
-export const isGlobalContest = (settings: ContestSettings | undefined) => {
+export const isGlobalContest = (settings: Pick<ContestSettings, 'startTimestamp' | 'frozenTimestamp' | 'quietTimestamp' | 'endTimestamp' | 'penalty'> | undefined) => {
   return settings?.startTimestamp === 0
     && settings?.frozenTimestamp === 0
     && settings?.quietTimestamp === 0
@@ -17,10 +17,10 @@ export const isGlobalContest = (settings: ContestSettings | undefined) => {
     && settings?.penalty === 0;
 };
 
-export const isPastContest = (settings: ContestSettings | undefined) => {
+export const isPastContest = (settings: Pick<ContestSettings, 'endTimestamp'> | undefined) => {
   return Date.now() > (settings?.endTimestamp ?? 0);
 };
 
-export const isFutureContest = (settings: ContestSettings | undefined) => {
+export const isFutureContest = (settings: Pick<ContestSettings, 'startTimestamp'> | undefined) => {
   return (settings?.startTimestamp ?? 0) > Date.now();
 };
