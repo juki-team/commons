8b35fd8689fe67ea0c8d871da3388ff58154899e
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-03-04 11:45:59 -0400

    adding getAssignmentUserCommentKey

--- src/helpers/comments.ts
+++ src/helpers/comments.ts
@@ -1,3 +1,4 @@
 export const getAssignmentCommentKey = (assignmentId: string) => `assignment:${assignmentId}`;
+export const getAssignmentUserCommentKey = (assignmentUserId: string) => `assignment-user:${assignmentUserId}`;
 export const getProblemCommentKey = (assignmentId: string) => `problem:${assignmentId}`;
 export const getContestCommentKey = (assignmentId: string) => `contest:${assignmentId}`;
