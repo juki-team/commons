ca9861daca17c1aa54058301c376f23c8cf7d6cd
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-03-05 18:18:52 -0400

    fixing getAssignmentUserCommentKey

--- src/helpers/comments.ts
+++ src/helpers/comments.ts
@@ -1,4 +1,4 @@
-export const getAssignmentCommentKey = (classKey: string, sessionKey: string, assignmentKey: string) => `class:${classKey}|session:${sessionKey}|assignment:${assignmentKey}`;
-export const getAssignmentUserCommentKey = (classKey: string, sessionKey: string, assignmentKey: string, userNickname: string, userCompany: string) => `class:${classKey}|session:${sessionKey}|assignment:${assignmentKey}|user_nickname:${userNickname}|user_company:${userCompany}`;
+export const getAssignmentCommentKey = (classKey: string, cycleKey: string, sessionKey: string, assignmentKey: string) => `class:${classKey}|cycle:${cycleKey}|session:${sessionKey}|assignment:${assignmentKey}`;
+export const getAssignmentUserCommentKey = (classKey: string, cycleKey: string, sessionKey: string, assignmentKey: string, userNickname: string, userCompany: string) => `class:${classKey}|cycle:${cycleKey}|session:${sessionKey}|assignment:${assignmentKey}|user_nickname:${userNickname}|user_company:${userCompany}`;
 export const getProblemCommentKey = (problemKey: string) => `problem:${problemKey}`;
 export const getContestCommentKey = (contestKey: string) => `contest:${contestKey}`;
