13e4ac062eaa12153cc084efa1069417cd3feb5a
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-03-05 18:16:51 -0400

    fixing getAssignmentUserCommentKey

--- src/helpers/comments.ts
+++ src/helpers/comments.ts
@@ -1,4 +1,4 @@
-export const getAssignmentCommentKey = (assignmentKey: string) => `assignment:${assignmentKey}`;
-export const getAssignmentUserCommentKey = (assignmentKey: string, userNickname: string, userCompany: string) => `assignment:${assignmentKey}|user_nickname:${userNickname}|user_company:${userCompany}`;
+export const getAssignmentCommentKey = (classKey: string, sessionKey: string, assignmentKey: string) => `class:${classKey}|session:${sessionKey}|assignment:${assignmentKey}`;
+export const getAssignmentUserCommentKey = (classKey: string, sessionKey: string, assignmentKey: string, userNickname: string, userCompany: string) => `class:${classKey}|session:${sessionKey}|assignment:${assignmentKey}|user_nickname:${userNickname}|user_company:${userCompany}`;
 export const getProblemCommentKey = (problemKey: string) => `problem:${problemKey}`;
 export const getContestCommentKey = (contestKey: string) => `contest:${contestKey}`;
