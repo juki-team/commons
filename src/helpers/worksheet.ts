8ceab1c1f440387b9b11578755929ff5e7260ba7
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-05-25 20:21:59 -0400

    adding type guards

--- src/helpers/worksheet.ts
+++ src/helpers/worksheet.ts
@@ -2,7 +2,13 @@ import { NEW_PAGE_SHEET } from '../constants/worksheet';
 import {
   BasicWorksheetType,
   BodyWorksheetType,
+  CodeEditorSheetType,
+  JkmdSheetType,
+  Judge,
   NewPageSheetType,
+  QuizOptionsSheetType,
+  QuizProblemSheetType,
+  QuizTextSheetType,
   SummaryWorksheetsInPages,
   WorksheetsInPages,
   WorksheetType,
@@ -70,3 +76,52 @@ export const getSummaryWorksheetsInPages = (initialSheets: BodyWorksheetType[]) 
 };
 
 export const getTotalExercisesOfSummaryWorksheetsInPages = (content: SummaryWorksheetsInPages) => content.reduce((sum, { content }) => sum + content.reduce((sum, { type }) => sum + +(type === WorksheetType.QUIZ_OPTIONS || type === WorksheetType.QUIZ_PROBLEM || type === WorksheetType.QUIZ_TEXT), 0), 0);
+
+export const isBasicWorksheetType = (value: any): value is BasicWorksheetType & any => {
+  return typeof value?.id == 'string' && value.id
+    && Object.values(WorksheetType).includes(value?.type as WorksheetType)
+    && typeof value?.title == 'string'
+    && typeof value?.points == 'number';
+};
+
+export const isJkmdSheetType = (value: any): value is JkmdSheetType => {
+  return isBasicWorksheetType(value)
+    && value?.type === WorksheetType.JK_MD
+    && typeof value?.content == 'string';
+};
+
+export const isCodeEditorSheetType = (value: any): value is CodeEditorSheetType => {
+  return isBasicWorksheetType(value)
+    && value?.type === WorksheetType.CODE_EDITOR
+    && typeof value?.sourceCode === 'object' && value?.sourceCode !== null
+    && value?.testCases === 'object' && value?.testCases !== null
+    && Array.isArray(value?.languages)
+    && typeof value?.height == 'number';
+};
+
+export const isQuizProblemSheetType = (value: any): value is QuizProblemSheetType => {
+  return isBasicWorksheetType(value)
+    && value?.type === WorksheetType.QUIZ_PROBLEM
+    && Object.values(Judge).includes(value?.problemJudge as Judge) || typeof value?.problemJudge === 'string'
+    && typeof value?.problemKey == 'string'
+    && Array.isArray(value?.languages)
+    && typeof value?.height == 'number';
+};
+
+export const isQuizOptionsSheetType = (value: any): value is QuizOptionsSheetType => {
+  return isBasicWorksheetType(value)
+    && value?.type === WorksheetType.QUIZ_OPTIONS
+    && typeof value?.description == 'string'
+    && Array.isArray(value?.options)
+    && value?.options.every((option: any) => typeof option?.label == 'string' && typeof option?.correct == 'boolean' && typeof option?.id == 'string')
+    && typeof value?.multiple == 'boolean'
+    && (value?.scoringMode === 'TOTAL' || value?.scoringMode === 'PARTIAL');
+};
+
+export const isQuizTextSheetType = (value: any): value is QuizTextSheetType => {
+  return isBasicWorksheetType(value)
+    && value?.type === WorksheetType.QUIZ_TEXT
+    && typeof value?.description == 'string'
+    && typeof value?.answer == 'string'
+    && [ 'text', 'number', 'textarea' ].includes(value?.inputType);
+};
