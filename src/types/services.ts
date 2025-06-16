7832bf8beca74ae871e9012e00ef24b0bb287ad1
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-06-16 10:40:01 -0400

    improving RunnerSQSMessageBodyType 2

--- src/types/services.ts
+++ src/types/services.ts
@@ -218,7 +218,7 @@ export type RunCommandType = {
 
 export type RunnerSQSMessageBodyType = {
   executions: RunCommandType[],
-  next: RunnerNextType,
+  next?: RunnerNextType,
 };
 
 export enum JudgingState {
