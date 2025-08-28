f88fa1c6a3a328b969b401b80cf127bcb349406d
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-08-28 18:27:12 -0400

    fixing getDocumentAccess

--- src/helpers/document.ts
+++ src/helpers/document.ts
@@ -14,17 +14,17 @@ export const getDocumentAccess = (document: { members: EntityMembers } | undefin
     && document?.members?.rankSpectators === EntityMembersRank.OPEN
     && document?.members?.rankParticipants === EntityMembersRank.NONE:
       return EntityAccess.EXPOSED;
-    case document?.members?.rankAdministrators === EntityMembersRank.NONE
+    case (document?.members?.rankAdministrators === EntityMembersRank.NONE || document?.members?.rankAdministrators === EntityMembersRank.CLOSE)
     && document?.members?.rankManagers === EntityMembersRank.CLOSE
-    && document?.members?.rankGuests === EntityMembersRank.NONE
+    && (document?.members?.rankGuests === EntityMembersRank.NONE || document?.members?.rankGuests === EntityMembersRank.CLOSE)
     && document?.members?.rankSpectators === EntityMembersRank.OPEN
-    && document?.members?.rankParticipants === EntityMembersRank.NONE:
+    && (document?.members?.rankParticipants === EntityMembersRank.NONE || document?.members?.rankParticipants === EntityMembersRank.CLOSE):
       return EntityAccess.PUBLIC;
-    case document?.members?.rankAdministrators === EntityMembersRank.NONE
+    case (document?.members?.rankAdministrators === EntityMembersRank.NONE || document?.members?.rankAdministrators === EntityMembersRank.CLOSE)
     && document?.members?.rankManagers === EntityMembersRank.CLOSE
-    && document?.members?.rankGuests === EntityMembersRank.NONE
+    && (document?.members?.rankGuests === EntityMembersRank.NONE || document?.members?.rankGuests === EntityMembersRank.CLOSE)
     && document?.members?.rankSpectators === EntityMembersRank.CLOSE
-    && document?.members?.rankParticipants === EntityMembersRank.NONE:
+    && (document?.members?.rankParticipants === EntityMembersRank.NONE || document?.members?.rankParticipants === EntityMembersRank.CLOSE):
       return EntityAccess.RESTRICTED;
     case document?.members?.rankAdministrators === EntityMembersRank.NONE
     && document?.members?.rankManagers === EntityMembersRank.NONE
@@ -36,7 +36,6 @@ export const getDocumentAccess = (document: { members: EntityMembers } | undefin
       return EntityAccess.PRIVATE;
   }
 };
-
 
 export const isUserMember = (member: { userId?: string, teamId?: string }): member is EntityUsersMemberUserData => {
   return typeof member.userId === 'string';
